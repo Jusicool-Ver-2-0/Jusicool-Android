6460792911a832b93803cae88f55de8648e09bed
jsw9436 <jswjsw101010@gmail.com>
2025-04-10 22:07:29 +0900

    :lipstick: :: Add JusicoolColor

---
+++ ui/design-system/src/main/java/com/jusicool/design_system/theme/color/JusicoolColor.kt
@@ -0,0 +1,23 @@
+package com.jusicool.design_system.theme.color
+
+import androidx.compose.ui.graphics.Color
+
+object JusicoolColor : ColorTheme() {
+
+    // MainColor
+    override val main = Color(0xFF2756F1)
+
+    // Error(Red)
+    override val error = Color(0xFFDF454A)
+
+    // Greyscale
+    override val gray800 = Color(0xFF3C3C3E)
+    override val gray700 = Color(0xFF4F4F51)
+    override val gray600 = Color(0xFF666669)
+    override val gray500 = Color(0xFF8F9094)
+    override val gray400 = Color(0xFFA5A6A9)
+    override val gray300 = Color(0xFFB4B5B7)
+    override val gray200 = Color(0xFFCBCCCE)
+    override val gray100 = Color(0xFFEFF0F2)
+    override val gray50 = Color(0xFFF5F6F8)
+}
