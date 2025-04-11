a4b922486a7a83fd823351287bf5390795d2980a
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-04-11 10:20:30 +0900

    🔥 :: build.gradle.kts 불필요한 의존성 삭제

--- data/di/build.gradle.kts
+++ data/di/build.gradle.kts
@@ -10,8 +10,6 @@ android {
 dependencies {
     // todo : Add Other Project Implementation -> ex) implementation(project(":core:___")) / (project(":feature:____"))
 
-    implementation(libs.kotlinx.datetime)
-    implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp.logging)
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.kotlin.serialization)
