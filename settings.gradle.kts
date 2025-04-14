09eca1138a1c75e9526bfc3bca1b5a1d7e0f7359
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-04-14 14:04:26 +0900

    ✨ :: add settings.gradle.kts 의존성 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,17 +23,22 @@ dependencyResolutionManagement {
 rootProject.name = "Jusicool-Android"
 include(":app")
 
-include(":domain")
 include(":feature")
 
 include(":ui")
 include(":ui:design-system")
 include(":ui:utils")
-
 include(":data")
+
 include(":data:network")
 include(":data:di")
 include(":data:local")
 include(":data:model")
 include(":data:repository")
 include(":data:utils")
+
+include(":domain")
+include(":domain:entity")
+include(":domain:repository")
+include(":domain:usecase")
+include(":domain:utils")
