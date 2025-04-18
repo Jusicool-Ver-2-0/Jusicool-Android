266c2dc61adf26f5e47ff0d220254ca27c6cf371
jsw9436 <jswjsw101010@gmail.com>
2025-04-18 13:39:35 +0900

    :recycle: :: 필요없는 import문 제거

--- ui/design-system/src/main/java/com/jusicool/design_system/component/button/JusicoolButton.kt
+++ ui/design-system/src/main/java/com/jusicool/design_system/component/button/JusicoolButton.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
