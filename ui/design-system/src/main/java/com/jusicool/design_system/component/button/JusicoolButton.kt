bed8899e919d3c0d4df1a0f7780f96b4f0f2c30c
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-04-29 16:44:40 +0900

    ✨ :: add button color

--- ui/design-system/src/main/java/com/jusicool/design_system/component/button/JusicoolButton.kt
+++ ui/design-system/src/main/java/com/jusicool/design_system/component/button/JusicoolButton.kt
@@ -1,13 +1,11 @@
 package com.jusicool.design_system.component.button
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
