de6925d2f732a70dd693d3897a73cafd80e9a0e9
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-05-19 10:49:08 +0900

    ✨ :: add ChartRepositoryModule

--- data/di/src/main/java/com/jusicool/di/RepositoryModule.kt
+++ data/di/src/main/java/com/jusicool/di/RepositoryModule.kt
@@ -1,5 +1,8 @@
 package com.jusicool.di
 
+import com.jusicool.repository.ChartRepositoryImpl
+import com.jusicool.repository.chart.ChartRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -8,4 +11,8 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
     // todo : Add Repository Instance
+    @Binds
+    abstract fun bindChartRepository(
+        chartRepositoryImpl: ChartRepositoryImpl
+    ): ChartRepository
 }
