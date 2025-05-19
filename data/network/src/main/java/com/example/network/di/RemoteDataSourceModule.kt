e4c75fa8b52f1498ee92150883f26c76f1179367
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-05-19 10:49:52 +0900

    ✨ :: add ChartDataSourceModule

--- data/network/src/main/java/com/example/network/di/RemoteDataSourceModule.kt
+++ data/network/src/main/java/com/example/network/di/RemoteDataSourceModule.kt
@@ -1,5 +1,8 @@
 package com.example.network.di
 
+import com.example.network.datasource.chart.ChartDataSource
+import com.example.network.datasource.chart.ChartDataSourceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -14,4 +17,8 @@ abstract class RemoteDataSourceModule {
     ) : AuthDataSource
 */
 
+    @Binds
+    abstract fun bindChartDataSource(
+        chartDataSourceImpl: ChartDataSourceImpl
+    ):ChartDataSource
 }
