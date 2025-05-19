f102d80c34179b8afdfba18e7a7fbdd1e1017bfb
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-05-19 10:49:26 +0900

    ✨ :: add annotaion

---
+++ data/network/src/main/java/com/example/network/util/RetrofitQuarify.kt
@@ -0,0 +1,11 @@
+package com.example.network.util
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class UpbitRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseApiRetrofit
