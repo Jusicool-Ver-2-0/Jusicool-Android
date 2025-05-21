68354703ecae7902eb8bef0d5092ff6ae1314ce1
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-05-21 09:11:39 +0900

    ✨ :: add BaseURL logic

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -1,12 +1,31 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("jusicool.android.core")
     id("jusicool.android.hilt")
 }
 android {
-    namespace = "com.example.network"
+    namespace = "com.jusicool.network"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        buildConfigField(
+            type = "String",
+            name = "BASE_URL",
+            getApiKey("BASE_URL")
+        )
+    }
 }
 
 dependencies {
+    debugImplementation(libs.debug.chuck)
+    releaseImplementation(libs.release.chuck)
+
+    implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp.logging)
     implementation(libs.retrofit.core)
@@ -17,4 +36,11 @@ dependencies {
 
     implementation(project(":data:model"))
     implementation(project(":data:utils"))
+}
+
+fun getApiKey(propertyKey: String) : String {
+    val propFile = rootProject.file("./local.properties")
+    val properties = Properties()
+    properties.load(FileInputStream(propFile))
+    return properties.getProperty(propertyKey) ?: throw IllegalArgumentException("Property $propertyKey not found in local.properties")
 }
