2f1c22d31e058c7673def2a0ef7985c81262fb8c
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-05-30 09:43:42 +0900

    ✨ :: state -> signUpState 변수 이름 변경

--- app/src/main/java/com/jusicool/jusicool_android/MainActivity.kt
+++ app/src/main/java/com/jusicool/jusicool_android/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jusicool.jusicool_android.ui.theme.JusicoolAndroidTheme
+import com.jusicool.signin.view.SignInRoute
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,22 +13,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            SignInRoute(
+                onSignInClick = {}
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JusicoolAndroidTheme {
-        Greeting("Android")
     }
 }
