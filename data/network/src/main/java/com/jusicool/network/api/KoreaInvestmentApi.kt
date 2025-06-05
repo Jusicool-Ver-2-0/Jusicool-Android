b9c73b8cf269c90c912ef277d1ffb2bfadd653d6
youloveli <mymhj122@gmail.com>
2025-06-05 09:26:16 +0900

    :sparkles: :: 인증 관련 api의 메서드르 post로 변경

--- data/network/src/main/java/com/jusicool/network/api/KoreaInvestmentApi.kt
+++ data/network/src/main/java/com/jusicool/network/api/KoreaInvestmentApi.kt
@@ -9,16 +9,17 @@ import com.jusicool.model.koreaInvestment.WebSocketAccessKeyResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface KoreaInvestmentApi {
 
-    @GET("/oauth2/Approval")
+    @POST("/oauth2/Approval")
     suspend fun getWebSocketAccessToken(
         @Body body: AccessKeyRequest,
     ): WebSocketAccessKeyResponse
 
-    @GET("/oauth2/tokenP")
+    @POST("/oauth2/tokenP")
     suspend fun getAccessToken(
         @Body body: AccessKeyRequest,
     ): AccessTokenResponse
