543b28b81938e31f2ca535c726874a32f9093bd3
youloveli <mymhj122@gmail.com>
2025-06-09 10:28:24 +0900

    :sparkles: :: KoreaInvestmentDataSource 추가

---
+++ data/network/src/main/java/com/jusicool/network/datasource/koreaInvestment/KoreaInvestmentDataSource.kt
@@ -0,0 +1,37 @@
+package com.jusicool.network.datasource.koreaInvestment
+
+import StockCandleResponse
+import StockPriceResponse
+import com.jusicool.model.koreaInvestment.AccessKeyRequest
+import com.jusicool.model.koreaInvestment.AccessTokenResponse
+import com.jusicool.model.koreaInvestment.StockMinutePriceResponse
+import com.jusicool.model.koreaInvestment.WebSocketAccessKeyResponse
+
+interface KoreaInvestmentDataSource {
+
+    suspend fun getAccessToken(request: AccessKeyRequest): AccessTokenResponse
+
+    suspend fun getWebSocketAccessToken(request: AccessKeyRequest): WebSocketAccessKeyResponse
+
+    suspend fun getStockOrder(
+        condMrktDivCode: String,
+        inputIsCd: String,
+        inputHour1: String,
+        inputDate1: String,
+        pwDataIncuYn: String,
+        fakeTickIncuYn: String? = null
+    ): StockCandleResponse
+
+    suspend fun getMinutePrice(
+        condMrktDivCode: String,
+        inputIsCd: String,
+        inputHour1: String,
+        pwDataIncuYn: String,
+        etcClsCode: String
+    ): StockMinutePriceResponse
+
+    suspend fun getStockCurrentPrice(
+        marketDivCode: String,
+        stockCode: String,
+    ): StockPriceResponse
+}
