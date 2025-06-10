06149eb129f264b7a100c4febcdba69ccb436b02
youloveli <mymhj122@gmail.com>
2025-06-10 19:41:37 +0900

    :recycle: :: StockSearchUiState 기본값 추가

--- feature/chart/src/main/java/com/jusicool/chart/viewModel/StockSearchUiState.kt
+++ feature/chart/src/main/java/com/jusicool/chart/viewModel/StockSearchUiState.kt
@@ -2,12 +2,13 @@ package com.jusicool.chart.viewModel
 
 import com.jusicool.chart.view.StockSearchTagData
 import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
 
 data class StockSearchUiState(
     val isLoading: Boolean = false,
-    val searchTextState: String,
-    val popularKeyword: String,
-    val resentSearchTagData: PersistentList<StockSearchTagData>,
-    val popularKeywordData: PersistentList<Pair<String, Double>>,
+    val searchTextState: String = "",
+    val popularKeyword: String = "",
+    val resentSearchTagData: PersistentList<StockSearchTagData> = persistentListOf(),
+    val popularKeywordData: PersistentList<Pair<String, Double>> = persistentListOf(),
     val errorMessage: String? = null,
 )
