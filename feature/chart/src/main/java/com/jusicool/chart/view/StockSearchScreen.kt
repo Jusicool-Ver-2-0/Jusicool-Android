698f0fe3d980fe025f1b3e7ea40475dd4d6681ba
youloveli <mymhj122@gmail.com>
2025-06-10 12:07:31 +0900

    :lipstick: :: SearchBox 퍼블리싱, screen 에 적용

--- feature/chart/src/main/java/com/jusicool/chart/view/StockSearchScreen.kt
+++ feature/chart/src/main/java/com/jusicool/chart/view/StockSearchScreen.kt
@@ -1,9 +1,13 @@
 package com.jusicool.chart.view
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,17 +15,62 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jusicool.design_system.component.modifier.JusicoolClickable
+import com.jusicool.design_system.component.textField.TransparentTextField
 import com.jusicool.design_system.theme.JusicoolTheme
 import com.jusicool.utils.FormatPercent
+import com.school_of_company.design_system.icon.ClarityArrowLineIcon
 
 @Composable
 internal fun StockSearchRoute(modifier: Modifier = Modifier) {
-    StockSearchScreen(modifier = modifier)
+
 }
 
 @Composable
-private fun StockSearchScreen(modifier: Modifier = Modifier) {
+private fun StockSearchScreen(
+    modifier: Modifier = Modifier,
+    searchTextState: String,
+    popularKeyword: String,
+    onSearchTextChange: (String) -> Unit,
+    popUpBackStack: () -> Unit
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        SearchBox(
+            modifier = Modifier.fillMaxWidth(),
+            searchTextState = searchTextState,
+            popularKeyword = popularKeyword,
+            onSearchTextChange = onSearchTextChange,
+            onArrowClick = popUpBackStack,
+        )
+    }
+}
 
+@Composable
+private fun SearchBox(
+    modifier: Modifier = Modifier,
+    searchTextState: String,
+    popularKeyword: String,
+    onSearchTextChange: (String) -> Unit,
+    onArrowClick: () -> Unit
+) {
+    Row(
+        modifier = modifier.padding(horizontal = 24.dp, vertical = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        ClarityArrowLineIcon(
+            modifier = Modifier
+                .size(24.dp)
+                .JusicoolClickable(onClick = onArrowClick),
+        )
+
+        TransparentTextField(
+            modifier = Modifier.fillMaxWidth(),
+            textState = searchTextState,
+            onTextChange = onSearchTextChange,
+            placeHolder = "'${popularKeyword}'를 검색해보세요",
+        )
+    }
 }
 
 @Composable
