e6b98b1742a95058e3220aa4ac20fa1b823d1acb
youloveli <mymhj122@gmail.com>
2025-06-11 14:21:45 +0900

    :bug: :: FormatPercent의 format 함수의 형식 지정자 문제 해결

--- ui/utils/src/main/java/com/jusicool/utils/FormatPercent.kt
+++ ui/utils/src/main/java/com/jusicool/utils/FormatPercent.kt
@@ -3,12 +3,12 @@ package com.jusicool.utils
 object FormatPercent {
 
     /**
-     * 숫자를 퍼밀(%) 형식으로 반환
-     * ex: +23.2‰, -1.0‰
+     * 숫자를 퍼센트(%) 형식으로 반환
+     * ex: +23.2%, -1.0%
      */
     fun format(value: Double): String {
         return String.format(
-            "%+.1f%",
+            "%+.1f%%",
             value
         )
     }
