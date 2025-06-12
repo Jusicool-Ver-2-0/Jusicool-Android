d9bbd6a0138fda778863fd119f08d5a6256ac80f
youloveli <mymhj122@gmail.com>
2025-06-12 11:43:57 +0900

    :sparkles: :: ChartListViewModel 구현

---
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/ChartListViewModel.kt
@@ -0,0 +1,14 @@
+package com.meister.investmentsearch.viewModel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+internal class ChartListViewModel @Inject constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(ChartListUiState(isLoading = true))
+    val uiState: StateFlow<ChartListUiState> = _uiState.asStateFlow()
+}
