696efb7523abef42b58852c3b975d1aaad03b1ce
youloveli <mymhj122@gmail.com>
2025-06-12 15:03:07 +0900

    :recycle: :: toSignedText 함수 간소화

--- ui/utils/src/main/java/com/jusicool/utils/toSignedText.kt
+++ ui/utils/src/main/java/com/jusicool/utils/toSignedText.kt
@@ -1,26 +1,7 @@
 package com.jusicool.utils
 
 
-fun Int.toSignedText(): String = when {
-    this > 0 -> "+$this"
-    this < 0 -> "$this"
-    else -> "+0"
-}
-
-fun Long.toSignedText(): String = when {
-    this > 0 -> "+$this"
-    this < 0 -> "$this"
-    else -> "+0"
-}
-
-fun Float.toSignedText(): String = when {
-    this > 0 -> "+$this"
-    this < 0 -> "$this"
-    else -> "+0"
-}
-
-fun Double.toSignedText(): String = when {
-    this > 0 -> "+$this"
-    this < 0 -> "$this"
-    else -> "+0"
-}
+fun Int.toSignedText(): String = String.format("%+d", this)
+fun Long.toSignedText(): String = String.format("%+d", this)
+fun Float.toSignedText(): String = String.format("%+.2f", this)
+fun Double.toSignedText(): String = String.format("%+.2f", this)
