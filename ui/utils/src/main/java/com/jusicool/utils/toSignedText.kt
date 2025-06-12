45034fa1111c44ca717bb2858265e6a4502f95c9
youloveli <mymhj122@gmail.com>
2025-06-12 13:55:56 +0900

    :recycle: :: toSignedText 함수가 값이 0일때도 부호를 붙이도록 수정

--- ui/utils/src/main/java/com/jusicool/utils/toSignedText.kt
+++ ui/utils/src/main/java/com/jusicool/utils/toSignedText.kt
@@ -4,23 +4,23 @@ package com.jusicool.utils
 fun Int.toSignedText(): String = when {
     this > 0 -> "+$this"
     this < 0 -> "$this"
-    else -> "0"
+    else -> "+0"
 }
 
 fun Long.toSignedText(): String = when {
     this > 0 -> "+$this"
     this < 0 -> "$this"
-    else -> "0"
+    else -> "+0"
 }
 
 fun Float.toSignedText(): String = when {
     this > 0 -> "+$this"
     this < 0 -> "$this"
-    else -> "0"
+    else -> "+0"
 }
 
 fun Double.toSignedText(): String = when {
     this > 0 -> "+$this"
     this < 0 -> "$this"
-    else -> "0"
+    else -> "+0"
 }
