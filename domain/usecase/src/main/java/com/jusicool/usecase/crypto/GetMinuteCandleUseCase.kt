30707e861151187a21990e1376987ff52f09532d
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-06-15 17:50:10 +0900

    ✨ :: 업비트 차트 api 연결

---
+++ domain/usecase/src/main/java/com/jusicool/usecase/crypto/GetMinuteCandleUseCase.kt
@@ -0,0 +1,12 @@
+package com.jusicool.usecase.crypto
+
+import com.jusicool.repository.crypto.CryptoRepository
+import javax.inject.Inject
+
+class GetMinuteCandleUseCase @Inject constructor(
+    private val cryptoRepository: CryptoRepository
+) {
+    operator fun invoke(market: String, to: String, count: Int) = runCatching {
+        cryptoRepository.getMinuteCandle(market = market, to = to, count = count)
+    }
+}
