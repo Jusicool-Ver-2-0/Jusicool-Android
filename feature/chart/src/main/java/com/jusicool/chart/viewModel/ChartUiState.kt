4cf835d3711ac06d288ade42c42eed4987b1ee60
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-06-15 17:48:32 +0900

    🔥 :: GetMinuteCandleUiState 삭제

--- feature/chart/src/main/java/com/jusicool/chart/viewModel/ChartUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.jusicool.chart.viewModel
-
-internal sealed class ChartUiState {
-    object Loading : ChartUiState()
-    class Success: ChartUiState()
-    data class Error(val message: String) : ChartUiState()
-}
