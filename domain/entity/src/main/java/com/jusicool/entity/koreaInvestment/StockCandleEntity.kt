cc629e9a9e99e47a11b8f0bb00393f196c38e5dc
youloveli <mymhj122@gmail.com>
2025-06-16 17:36:41 +0900

    :fire: :: 거래량 삭제

--- domain/entity/src/main/java/com/jusicool/entity/koreaInvestment/StockCandleEntity.kt
+++ domain/entity/src/main/java/com/jusicool/entity/koreaInvestment/StockCandleEntity.kt
@@ -6,13 +6,11 @@ data class StockCandleEntity(
     val closePrice: Long,
     val highPrice: Long,
     val lowPrice: Long,
-    val volume: Long
 ) {
     init {
         require(openPrice > 0) { "시작가는 0보다 커야 합니다." }
         require(closePrice > 0) { "마감가는 0보다 커야 합니다." }
         require(highPrice >= listOf(openPrice, closePrice, lowPrice).max()) { "최고가는 open/close/low 중 가장 높아야 합니다." }
         require(lowPrice <= listOf(openPrice, closePrice, highPrice).min()) { "최저가는 open/close/high 중 가장 낮아야 합니다." }
-        require(volume >= 0) { "거래량은 음수가 될 수 없습니다." }
     }
 }
