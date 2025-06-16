a7a6ed36f65213faf406e54fcc9cca500fded519
youloveli <mymhj122@gmail.com>
2025-06-16 14:37:14 +0900

    :sparkles: :: 401 응답 시 Authenticator에서 강제 토큰 재발급

--- data/network/src/main/java/com/jusicool/network/util/KoreaInvestmentAuthInterceptor.kt
+++ data/network/src/main/java/com/jusicool/network/util/KoreaInvestmentAuthInterceptor.kt
@@ -1,27 +1,44 @@
 package com.jusicool.network.util
 
+import android.util.Log
 import kotlinx.coroutines.runBlocking
-import okhttp3.Interceptor
+import okhttp3.Authenticator
+import okhttp3.Request
 import okhttp3.Response
+import okhttp3.Route
 
-class KoreaInvestmentAuthInterceptor(
+class KoreaInvestmentAuthenticator(
     private val koreaInvestmentAuthManager: KoreaInvestmentAuthManager
-) : Interceptor {
+) : Authenticator {
 
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-
-        val needsAuth = request.url.encodedPath.startsWith("/uapi/")
-
-        val newRequest = if (needsAuth) {
-            val token = runBlocking { koreaInvestmentAuthManager.getValidAccessToken() }
-            request.newBuilder()
-                .addHeader("Authorization", "Bearer $token")
-                .build()
-        } else {
-            request
+    override fun authenticate(route: Route?, response: Response): Request? {
+        if (responseCount(response) >= 2) {
+            return null
         }
 
-        return chain.proceed(newRequest)
+        val token = try {
+            runBlocking {
+                koreaInvestmentAuthManager.getValidAccessToken(forceRefresh = true)
+            }
+        } catch (e: Exception) {
+            Log.e("KoreaInvestmentAuth", "Token refresh failed", e)
+            null
+        }
+
+        return token?.let {
+            response.request.newBuilder()
+                .header("Authorization", "Bearer $it")
+                .build()
+        }
+    }
+
+    private fun responseCount(response: Response): Int {
+        var count = 1
+        var prior = response.priorResponse
+        while (prior != null) {
+            count++
+            prior = prior.priorResponse
+        }
+        return count
     }
 }
