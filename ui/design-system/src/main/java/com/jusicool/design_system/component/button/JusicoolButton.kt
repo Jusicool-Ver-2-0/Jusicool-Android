a8e2269c989aba8a0174ebd414bc09b5a4e8beac
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-06-16 21:06:34 +0900

    ✨ :: OutLine 버튼 추가

--- ui/design-system/src/main/java/com/jusicool/design_system/component/button/JusicoolButton.kt
+++ ui/design-system/src/main/java/com/jusicool/design_system/component/button/JusicoolButton.kt
@@ -1,11 +1,13 @@
 package com.jusicool.design_system.component.button
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -54,6 +56,33 @@ fun JusicoolFilledButton(
     }
 }
 
+@Composable
+fun JusicoolOutlinedButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    outlineColor: Color,
+    onClick: () -> Unit,
+) {
+    JusicoolTheme { colors, typography ->
+        OutlinedButton(
+            modifier = modifier,
+            onClick = onClick,
+            shape = RoundedCornerShape(12.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
+            border = BorderStroke(
+                width = 1.dp,
+                color = outlineColor
+            )
+        ) {
+            Text(
+                text = text,
+                color = outlineColor,
+                style = typography.bodyMedium
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun JusicoolButtonPreview() {
@@ -73,6 +102,11 @@ private fun JusicoolButtonPreview() {
             JusicoolFilledButton(
                 text = "다음",
                 state = ButtonState.Disable,
+                onClick = {}
+            )
+            JusicoolOutlinedButton(
+                text = "다음",
+                outlineColor = colors.error,
                 onClick = {}
             )
         }
