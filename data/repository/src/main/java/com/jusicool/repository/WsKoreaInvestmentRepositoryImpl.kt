6ae63b0d78e94213dbbcf71beab448178996e785
youloveli <mymhj122@gmail.com>
2025-06-17 12:56:32 +0900

    :recycle: :: WsKoreaInvestmentRepositoryImpl에서 observeStockTicker의 반환값 변경

--- data/repository/src/main/java/com/jusicool/repository/WsKoreaInvestmentRepositoryImpl.kt
+++ data/repository/src/main/java/com/jusicool/repository/WsKoreaInvestmentRepositoryImpl.kt
@@ -1,16 +1,18 @@
 package com.jusicool.repository
 
-import com.jusicool.model.koreaInvestment.ws.StockPriceSummary
+import com.jusicool.entity.koreaInvestment.StockPriceEntity
+import com.jusicool.model.mapper.koreaInvestment.toEntity
 import com.jusicool.network.datasource.koreaInvestment.ws.KoreaInvestmentWebSocketManagerInterface
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class WsKoreaInvestmentRepositoryImpl @Inject constructor(
     private val webSocketManager: KoreaInvestmentWebSocketManagerInterface
 ) : WsKoreaInvestmentRepository {
 
-    override fun observeStockTicker(): Flow<StockPriceSummary> {
-        return webSocketManager.stockTickerFlow
+    override fun observeStockTicker(): Flow<StockPriceEntity> {
+        return webSocketManager.stockTickerFlow.map { it.toEntity() }
     }
 
     override fun connectToStockTicker(trId: String, stockCode: String) {
--- domain/repository/src/main/java/com/jusicool/repository/WsKoreaInvestmentRepository.kt
+++ domain/repository/src/main/java/com/jusicool/repository/WsKoreaInvestmentRepository.kt
@@ -1,10 +1,10 @@
 package com.jusicool.repository
 
-import com.jusicool.model.koreaInvestment.ws.StockPriceSummary
+import com.jusicool.entity.koreaInvestment.StockPriceEntity
 import kotlinx.coroutines.flow.Flow
 
 interface WsKoreaInvestmentRepository {
-    fun observeStockTicker(): Flow<StockPriceSummary>
+    fun observeStockTicker(): Flow<StockPriceEntity>
     fun connectToStockTicker(trId: String, stockCode: String)
     fun disconnectFromStockTicker()
 }
