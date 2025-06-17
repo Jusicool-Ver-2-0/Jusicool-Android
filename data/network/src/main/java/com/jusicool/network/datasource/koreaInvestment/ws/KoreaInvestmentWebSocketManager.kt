a8d916e32aa457b07700d450c913f9fb655fd961
youloveli <mymhj122@gmail.com>
2025-06-17 09:33:19 +0900

    :recycle: :: KoreaInvestmentWebSocketManager의 토큰을 KoreaInvestmentAuthManager를 사용해서 관리하도록 수정

--- data/network/src/main/java/com/jusicool/network/datasource/koreaInvestment/ws/KoreaInvestmentWebSocketManager.kt
+++ data/network/src/main/java/com/jusicool/network/datasource/koreaInvestment/ws/KoreaInvestmentWebSocketManager.kt
@@ -1,6 +1,7 @@
 package com.jusicool.network.datasource.koreaInvestment.ws
 
 import com.jusicool.model.koreaInvestment.ws.StockPriceSummary
+import com.jusicool.network.util.KoreaInvestmentAuthManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -16,7 +17,8 @@ import javax.inject.Inject
 import javax.inject.Named
 
 class KoreaInvestmentWebSocketManager @Inject constructor(
-    @Named("koreaInvestmentOkHttpClient") private val client: OkHttpClient
+    @Named("koreaInvestmentOkHttpClient") private val client: OkHttpClient,
+    private val authManager: KoreaInvestmentAuthManager
 ) : KoreaInvestmentWebSocketManagerInterface {
 
     private var webSocket: WebSocket? = null
@@ -26,41 +28,47 @@ class KoreaInvestmentWebSocketManager @Inject constructor(
 
     private val scope = CoroutineScope(Dispatchers.IO)
 
-    override fun connect(
-        approvalKey: String,
-        trId: String,
-        stockCode: String
-    ) {
+    override fun connect(trId: String, stockCode: String) {
         if (webSocket != null) {
             disconnect()
         }
 
-        val request = Request.Builder()
-            .url("wss://realdata.koreainvestment.com:443/websocket")
-            .addHeader("Authorization", approvalKey)
-            .build()
+        scope.launch {
+            runCatching {
+                authManager.getApprovalKey()
+            }.onSuccess { approvalKey ->
+                val request = Request.Builder()
+                    .url("wss://realdata.koreainvestment.com:443/websocket")
+                    .addHeader("Authorization", approvalKey)
+                    .build()
 
-        webSocket = client.newWebSocket(request, object : WebSocketListener() {
-            override fun onOpen(ws: WebSocket, response: Response) {
-                val subscribeMsg = createSubscribeMessage(trId, stockCode)
-                ws.send(subscribeMsg)
-            }
+                webSocket = client.newWebSocket(request, object : WebSocketListener() {
+                    override fun onOpen(ws: WebSocket, response: Response) {
+                        val msg = createSubscribeMessage(trId, stockCode)
+                        ws.send(msg)
+                    }
 
-            override fun onMessage(ws: WebSocket, text: String) {
-                parseAndEmit(text)
-            }
+                    override fun onMessage(ws: WebSocket, text: String) {
+                        parseAndEmit(text)
+                    }
 
-            override fun onClosing(ws: WebSocket, code: Int, reason: String) {
-                ws.close(1000, null)
-                webSocket = null
-            }
+                    override fun onClosing(ws: WebSocket, code: Int, reason: String) {
+                        ws.close(1000, null)
+                        webSocket = null
+                    }
 
-            override fun onFailure(ws: WebSocket, t: Throwable, response: Response?) {
-                webSocket = null
-                // TODO: 에러 처리 구현
+                    override fun onFailure(ws: WebSocket, t: Throwable, response: Response?) {
+                        webSocket = null
+                        // TODO: 로그 및 재시도 처리
+                    }
+                })
+            }.onFailure { e ->
+                e.printStackTrace()
+                // TODO: 오류 알림, 재시도, UI 표시 등
             }
-        })
+        }
     }
+
 
     override fun disconnect() {
         webSocket?.close(1000, "Client disconnect")
--- data/network/src/main/java/com/jusicool/network/datasource/koreaInvestment/ws/KoreaInvestmentWebSocketManagerInterface.kt
+++ data/network/src/main/java/com/jusicool/network/datasource/koreaInvestment/ws/KoreaInvestmentWebSocketManagerInterface.kt
@@ -6,6 +6,6 @@ import kotlinx.coroutines.flow.SharedFlow
 interface KoreaInvestmentWebSocketManagerInterface {
     val stockTickerFlow: SharedFlow<StockPriceSummary>
 
-    fun connect(approvalKey: String, trId: String, stockCode: String)
+    fun connect(trId: String, stockCode: String)
     fun disconnect()
 }
