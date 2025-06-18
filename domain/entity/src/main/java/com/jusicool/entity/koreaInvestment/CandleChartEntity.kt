16c67cf126599910cb72502c65dc100491f70eb0
youloveli <mymhj122@gmail.com>
2025-06-18 15:47:32 +0900

    :recycle: :: StockCandleEntity의 파일명을 CandleChartEntity 로 변경

--- domain/entity/src/main/java/com/jusicool/entity/koreaInvestment/CandleChartEntity.kt
+++ domain/entity/src/main/java/com/jusicool/entity/koreaInvestment/CandleChartEntity.kt
@@ -3,12 +3,12 @@ package com.jusicool.entity.koreaInvestment
 /**
  * 주식 차트 캔들 데이터 (1분/일봉/주봉 등 공통 구조)
  */
-data class StockCandleEntity(
     val dateTime: String,     // 날짜 및 시간 (예: "20250614", "20250614-0930")
     val openPrice: Long,      // 시가
     val closePrice: Long,     // 종가
     val highPrice: Long,      // 고가
     val lowPrice: Long        // 저가
+data class CandleChartEntity(
 ) {
     init {
         require(openPrice > 0) { "시가(openPrice)는 0보다 커야 합니다." }
