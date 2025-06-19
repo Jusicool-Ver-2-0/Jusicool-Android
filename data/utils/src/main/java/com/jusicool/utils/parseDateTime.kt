2268e6c4974b8f03bb720ac37d260f239d1e0ee1
youloveli <mymhj122@gmail.com>
2025-06-19 12:07:47 +0900

    :recycle: :: yyyyMMddHHmm형식이 아니라 yyyyMMddHHmmss 형식으로 받도록 수정

--- data/utils/src/main/java/com/jusicool/utils/parseDateTime.kt
+++ data/utils/src/main/java/com/jusicool/utils/parseDateTime.kt
@@ -4,7 +4,6 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun parseDateTime(date: String, time: String): LocalDateTime {
-    // 예: 20240618 + 0930 → 2024-06-18T09:30
-    val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm")
-    return LocalDateTime.parse(date + time.padStart(4, '0'), formatter)
+    val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+    return LocalDateTime.parse(date + time.padStart(6, '0'), formatter)
 }
