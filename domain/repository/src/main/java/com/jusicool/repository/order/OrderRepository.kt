bb3ccd82e2dedce83e845735b667a2ca78354b23
youloveli <mymhj122@gmail.com>
2025-06-23 15:21:19 +0900

    :sparkles: :: OrderRepositoryImpl에 getOrderHistory 함수를 추가하고 변환 로직 추가

--- domain/repository/src/main/java/com/jusicool/repository/order/OrderRepository.kt
+++ domain/repository/src/main/java/com/jusicool/repository/order/OrderRepository.kt
@@ -1,8 +1,10 @@
 package com.jusicool.repository.order
 
 import com.jusicool.entity.order.OrderModel
+import com.jusicool.entity.orderHistory.OrderHistory
 import kotlinx.coroutines.flow.Flow
 
 interface OrderRepository {
     fun getMonthOrder(): Flow<OrderModel>
+    fun getOrderHistory(type: String): Flow<List<OrderHistory>>
 }
