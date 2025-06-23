0e77afc2b1c6d42ff263d0318cc76249c365efd8
youloveli <mymhj122@gmail.com>
2025-06-23 15:15:09 +0900

    :lipstick: :: OrderApi에 getOrderHistory함수 추가

--- data/network/src/main/java/com/jusicool/network/api/OrderApi.kt
+++ data/network/src/main/java/com/jusicool/network/api/OrderApi.kt
@@ -1,10 +1,16 @@
 package com.jusicool.network.api
 
-import com.jusicool.model.account.AccountResponse
+import com.jusicool.model.order.OrderHistoryResponse
 import com.jusicool.model.order.OrderResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface OrderApi {
     @GET("/order/month")
     suspend fun getMonthOrder(): OrderResponse
+
+    @GET("/order/my")
+    suspend fun getOrderHistory(
+        @Query("type") type: String,
+    ): List<OrderHistoryResponse>
 }
