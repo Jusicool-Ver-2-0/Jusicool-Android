c64c79a2ac363a49100de8eda1b064460090db19
youloveli <mymhj122@gmail.com>
2025-06-23 10:03:42 +0900

    :heavy_plus_sign: :: orderHistory 모듈 추가

---
+++ feature/orderHistory/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("jusicool.android.feature")
+    id("jusicool.android.hilt")
+}
+
+android {
+    namespace = "com.meister.orderhistory"
+}
+
+dependencies {
+    implementation(project(":domain:usecase"))
+    implementation(project(":domain:entity"))
+    implementation(project(":ui:utils"))
+}
