5ce6f932277d4ddd046d86eab3207c6e9733daa0
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-06-24 10:35:05 +0900

    ✨ :: 판매,구매, 예약 api 연결

--- data/repository/src/main/java/com/jusicool/repository/order/OrderRepositoryImpl.kt
+++ data/repository/src/main/java/com/jusicool/repository/order/OrderRepositoryImpl.kt
@@ -1,8 +1,14 @@
 package com.jusicool.repository.order
 
+import com.jusicool.entity.order.BuyRequestModel
+import com.jusicool.entity.order.BuyReserveModel
+import com.jusicool.entity.order.BuyResponseModel
 import com.jusicool.entity.order.OrderModel
+import com.jusicool.entity.order.SellRequestModel
+import com.jusicool.entity.order.SellReserveModel
+import com.jusicool.entity.order.SellResponseModel
+import com.jusicool.model.mapper.order.toDto
 import com.jusicool.model.mapper.order.toModel
-import com.jusicool.model.order.OrderResponse
 import com.jusicool.network.datasource.order.OrderDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -13,5 +19,21 @@ class OrderRepositoryImpl @Inject constructor(
 ):OrderRepository {
     override fun getMonthOrder(): Flow<OrderModel> {
         return orderDataSource.getMonthOrder().map { it.toModel() }
+    }
+
+    override fun postBuy(marketCode: String, quantity: BuyRequestModel): Flow<BuyResponseModel> {
+        return orderDataSource.postBuy(marketCode = marketCode, quantity = quantity.toDto()).map { it.toModel() }
+    }
+
+    override fun postSell(marketCode: String, quantity: SellRequestModel): Flow<SellResponseModel> {
+        return orderDataSource.postSell(marketCode = marketCode, quantity = quantity.toDto()).map { it.toModel() }
+    }
+
+    override fun postReserveBuy(marketCode: String, body: BuyReserveModel): Flow<Unit> {
+        return orderDataSource.postReserveBuy(marketCode = marketCode, body = body.toDto())
+    }
+
+    override fun postReserveSell(marketCode: String, body: SellReserveModel): Flow<Unit> {
+        return orderDataSource.postReserveSell(marketCode = marketCode, body = body.toDto())
     }
 }
