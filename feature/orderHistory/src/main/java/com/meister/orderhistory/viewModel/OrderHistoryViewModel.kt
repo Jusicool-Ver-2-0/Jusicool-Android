ad59256c81ab5513e3c4bf67ba9c027f9a13a26d
youloveli <mymhj122@gmail.com>
2025-06-25 14:48:51 +0900

    :recycle: :: 트리거의 타입을 MutableStateFlow에서 MutableSharedFlow으로 변경

--- feature/orderHistory/src/main/java/com/meister/orderhistory/viewModel/OrderHistoryViewModel.kt
+++ feature/orderHistory/src/main/java/com/meister/orderhistory/viewModel/OrderHistoryViewModel.kt
@@ -5,12 +5,11 @@ import androidx.lifecycle.viewModelScope
 import com.jusicool.entity.orderHistory.OrderHistory
 import com.jusicool.entity.orderHistory.OrderHistoryType
 import com.jusicool.usecase.order.GetOrderHistoryUseCase
-import com.jusicool.utils.Logger
 import com.meister.orderhistory.viewModel.uiState.CompletedOrderHistoryUiState
 import com.meister.orderhistory.viewModel.uiState.ReservedOrderHistoryUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.toPersistentList
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
@@ -18,6 +17,7 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,10 +25,11 @@ internal class OrderHistoryViewModel @Inject constructor(
     private val getOrderHistoryUseCase: GetOrderHistoryUseCase
 ) : ViewModel() {
 
-    private val reservedRefreshTrigger = MutableStateFlow(0)
+    private val reservedRefreshTrigger = MutableSharedFlow<Unit>()
 
-    internal val reservedOrderHistoryUiState: StateFlow<ReservedOrderHistoryUiState> =
+    val reservedOrderHistoryUiState: StateFlow<ReservedOrderHistoryUiState> =
         reservedRefreshTrigger
+            .onStart { emit(Unit) }
             .flatMapLatest {
                 getOrderHistoryUseCase(OrderHistoryType.RESERVE)
                     .map<List<OrderHistory>, ReservedOrderHistoryUiState> {
@@ -39,11 +40,9 @@ internal class OrderHistoryViewModel @Inject constructor(
                         )
                     }
                     .onStart {
-                        Logger.d("reservedOrderHistoryUiState", "데이터 로딩 시작")
                         emit(ReservedOrderHistoryUiState(isLoading = true))
                     }
                     .catch { e ->
-                        Logger.e("reservedOrderHistoryUiState", "에러 발생: ${e.message}", e)
                         emit(
                             ReservedOrderHistoryUiState(
                                 isLoading = false,
@@ -53,21 +52,23 @@ internal class OrderHistoryViewModel @Inject constructor(
                     }
             }
             .stateIn(
-                viewModelScope,
-                SharingStarted.WhileSubscribed(5_000),
-                ReservedOrderHistoryUiState(isLoading = true)
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+                initialValue = ReservedOrderHistoryUiState(isLoading = true)
             )
 
-
-    internal fun refreshReservedOrders() {
-        reservedRefreshTrigger.value += 1
-        Logger.d("refreshReservedOrders", "Count : ${reservedRefreshTrigger.value}")
+    fun refreshReservedOrders() {
+        viewModelScope.launch {
+            reservedRefreshTrigger.emit(Unit)
+        }
     }
 
-    private val completedRefreshTrigger = MutableStateFlow(0)
 
-    internal val completedOrderHistoryUiState: StateFlow<CompletedOrderHistoryUiState> =
+    private val completedRefreshTrigger = MutableSharedFlow<Unit>()
+
+    val completedOrderHistoryUiState: StateFlow<CompletedOrderHistoryUiState> =
         completedRefreshTrigger
+            .onStart { emit(Unit) }
             .flatMapLatest {
                 getOrderHistoryUseCase(OrderHistoryType.COMPLETED)
                     .map<List<OrderHistory>, CompletedOrderHistoryUiState> {
@@ -78,11 +79,9 @@ internal class OrderHistoryViewModel @Inject constructor(
                         )
                     }
                     .onStart {
-                        Logger.d("completedOrderHistoryUiState", "데이터 로딩 시작")
                         emit(CompletedOrderHistoryUiState(isLoading = true))
                     }
                     .catch { e ->
-                        Logger.e("completedOrderHistoryUiState", "에러 발생: ${e.message}", e)
                         emit(
                             CompletedOrderHistoryUiState(
                                 isLoading = false,
@@ -92,13 +91,14 @@ internal class OrderHistoryViewModel @Inject constructor(
                     }
             }
             .stateIn(
-                viewModelScope,
-                SharingStarted.WhileSubscribed(5_000),
-                CompletedOrderHistoryUiState(isLoading = true)
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+                initialValue = CompletedOrderHistoryUiState(isLoading = true)
             )
 
-    internal fun refreshCompletedOrders() {
-        completedRefreshTrigger.value += 1
-        Logger.d("refreshCompletedOrders", "Count : ${completedRefreshTrigger.value}")
+    fun refreshCompletedOrders() {
+        viewModelScope.launch {
+            completedRefreshTrigger.emit(Unit)
+        }
     }
 }
