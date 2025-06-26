9fc2fa9829872baa10b5a7f8cffde0872ef407f6
youloveli <mymhj122@gmail.com>
2025-06-26 20:11:12 +0900

    :green_heart: :: JusicoolDataBase 주석처리

--- data/local/src/main/java/com/jusicool/local/JusicoolDataBase.kt
+++ data/local/src/main/java/com/jusicool/local/JusicoolDataBase.kt
@@ -3,5 +3,5 @@ package com.jusicool.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [], version = 0)
-abstract class JusicoolDataBase : RoomDatabase() {}
+// @Database(entities = [], version = 0)
+// abstract class JusicoolDataBase : RoomDatabase() {}
