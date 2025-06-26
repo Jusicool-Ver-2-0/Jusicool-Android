532e14c0cb45ff952813beb03fcc389217e6b41d
youloveli <mymhj122@gmail.com>
2025-06-26 15:15:16 +0900

    :sparkles: :: MarketListResponse 에서 Market으로 변환해주는 toEntity 함수 추가

---
+++ data/model/src/main/java/com/jusicool/model/mapper/market/MarketListResponseMapper.kt
@@ -0,0 +1,13 @@
+package com.jusicool.model.mapper.market
+
+import com.jusicool.entity.market.Market
+import com.jusicool.entity.market.MarketType
+import com.jusicool.model.market.MarketListResponse
+
+fun MarketListResponse.toEntity() = Market(
+    id = this.id,
+    koreanName = this.koreanName,
+    englishName = this.englishName,
+    market = this.market,
+    marketType = MarketType.valueOf(this.marketType),
+)
