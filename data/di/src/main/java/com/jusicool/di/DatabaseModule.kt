5d1194d2c8f4d30a562ccf485f28dda112c7123d
youloveli <mymhj122@gmail.com>
2025-06-26 20:14:48 +0900

    :green_heart: :: DatabaseModule 주석처리

--- data/di/src/main/java/com/jusicool/di/DatabaseModule.kt
+++ data/di/src/main/java/com/jusicool/di/DatabaseModule.kt
@@ -2,7 +2,7 @@ package com.jusicool.di
 
 import android.content.Context
 import androidx.room.Room
-import com.jusicool.local.JusicoolDataBase
+// import com.jusicool.local.JusicoolDataBase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,13 +12,13 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
-    @Provides
-    @Singleton
-    fun provideJusicoolDatabase(appContext: Context): JusicoolDataBase {
-        return Room.databaseBuilder(
-            appContext,
-            JusicoolDataBase::class.java,
-            "jusicool_database"
-        ).build()
-    }
+//    @Provides
+//    @Singleton
+//    fun provideJusicoolDatabase(appContext: Context): JusicoolDataBase {
+//        return Room.databaseBuilder(
+//            appContext,
+//            JusicoolDataBase::class.java,
+//            "jusicool_database"
+//        ).build()
+//    }
 }
