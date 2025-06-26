6384ca9135765167d7f9518c0e830bac479455ef
youloveli <mymhj122@gmail.com>
2025-06-26 18:03:24 +0900

    :sparkles: :: room 의존성 추가, data:local 모듈에 의존성 추가

---
+++ data/local/src/main/java/com/jusicool/local/JusicoolDataBase.kt
@@ -0,0 +1,7 @@
+package com.jusicool.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 0)
+abstract class JusicoolDataBase : RoomDatabase() {}
