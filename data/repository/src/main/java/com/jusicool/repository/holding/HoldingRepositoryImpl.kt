4c30598beab7a5098f1b98840d09ea8978de6fff
youloveli <mymhj122@gmail.com>
2025-06-27 12:11:22 +0900

    :sparkles: :: observeHoldings 함수가 값들을 DB에서 불러오도록 수정, refreshHoldings 함수 추가

--- data/repository/src/main/java/com/jusicool/repository/holding/HoldingRepositoryImpl.kt
+++ data/repository/src/main/java/com/jusicool/repository/holding/HoldingRepositoryImpl.kt
@@ -1,16 +1,35 @@
 package com.jusicool.repository.holding
 
 import com.jusicool.entity.holding.HoldingModel
-import com.jusicool.model.mapper.holding.toModel
+import com.jusicool.local.dao.HoldingDao
+import com.jusicool.local.dao.MarketDao
+import com.jusicool.local.util.mapper.toDomain.toModel
+import com.jusicool.local.util.mapper.toLocal.toEntities
 import com.jusicool.network.datasource.holding.HoldingDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class HoldingRepositoryImpl @Inject constructor(
-    private val holdingDataSource: HoldingDataSource
-): HoldingRepository{
-    override fun getHolding(): Flow<List<HoldingModel>> {
-        return holdingDataSource.getHolding().map { list -> list.map { it.toModel() } }
+    private val holdingDataSource: HoldingDataSource,
+    private val holdingDao: HoldingDao,
+    private val marketDao: MarketDao,
+) : HoldingRepository {
+
+    override fun observeHoldings(): Flow<List<HoldingModel>> {
+        return holdingDao.observeAllHoldingsWithMarket()
+            .map { list -> list.map { it.toModel() } }
+    }
+
+    override suspend fun refreshHoldings() {
+        holdingDataSource.getHolding()
+            .collect { holdingResponseList ->
+                holdingResponseList.forEach { holdingResponse ->
+                    val (holdingEntity, marketEntity) = holdingResponse.toEntities()
+
+                    marketDao.insertMarket(marketEntity)
+                    holdingDao.insertHolding(holdingEntity)
+                }
+            }
     }
 }
