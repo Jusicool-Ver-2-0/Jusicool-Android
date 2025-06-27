e07fa5e28c588841ec5da0ca1adb8c707d37a592
youloveli <mymhj122@gmail.com>
2025-06-27 12:06:03 +0900

    :heavy_plus_sign: :: local -> entity 의존성 추가

--- data/local/build.gradle.kts
+++ data/local/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     ksp(libs.room.compiler)
     implementation(libs.room.ktx)
 
+    implementation(project(":domain:entity"))
     implementation(project(":data:model"))
     implementation(project(":data:utils"))
 }
