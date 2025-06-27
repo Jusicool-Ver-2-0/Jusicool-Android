e5437d33c8d2531d00fb8cc40dbd8305d7f0ad86
youloveli <mymhj122@gmail.com>
2025-06-27 10:09:09 +0900

    :sparkles: :: JusicoolDataBase에 주석 제거, dao 등록

--- data/local/src/main/java/com/jusicool/local/JusicoolDataBase.kt
+++ data/local/src/main/java/com/jusicool/local/JusicoolDataBase.kt
@@ -2,6 +2,20 @@ package com.jusicool.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.jusicool.local.dao.HoldingDao
+import com.jusicool.local.dao.MarketDao
+import com.jusicool.local.entity.HoldingEntity
+import com.jusicool.local.entity.MarketEntity
 
-// @Database(entities = [], version = 0)
-// abstract class JusicoolDataBase : RoomDatabase() {}
+@Database(
+    entities = [
+        HoldingEntity::class,
+        MarketEntity::class,
+    ], version = 0
+)
+abstract class JusicoolDataBase : RoomDatabase() {
+
+    abstract fun holdingDao(): HoldingDao
+
+    abstract fun marketDao(): MarketDao
+}
