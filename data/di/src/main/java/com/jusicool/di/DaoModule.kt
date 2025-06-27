f54d9aa455764a80efcd500980ec689ea7165579
youloveli <mymhj122@gmail.com>
2025-06-27 10:07:30 +0900

    :fire: :: 주석 삭제

--- data/di/src/main/java/com/jusicool/di/DaoModule.kt
+++ data/di/src/main/java/com/jusicool/di/DaoModule.kt
@@ -1,6 +1,10 @@
 package com.jusicool.di
 
+import com.jusicool.local.JusicoolDataBase
+import com.jusicool.local.dao.HoldingDao
+import com.jusicool.local.dao.MarketDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
@@ -8,8 +12,6 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object DaoModule {
-//    @Provides
-//    fun provideStockDao(database: AppDatabase): StockDao {
-//        return database.stockDao()
-//    }
+
+
 }
--- data/di/src/main/java/com/jusicool/di/DatabaseModule.kt
+++ data/di/src/main/java/com/jusicool/di/DatabaseModule.kt
@@ -1,24 +1,24 @@
-package com.jusicool.di
+    package com.jusicool.di
 
-import android.content.Context
-import androidx.room.Room
-// import com.jusicool.local.JusicoolDataBase
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
+    import android.content.Context
+    import androidx.room.Room
+    import com.jusicool.local.JusicoolDataBase
+    import dagger.Module
+    import dagger.Provides
+    import dagger.hilt.InstallIn
+    import dagger.hilt.components.SingletonComponent
+    import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-object DatabaseModule {
-//    @Provides
-//    @Singleton
-//    fun provideJusicoolDatabase(appContext: Context): JusicoolDataBase {
-//        return Room.databaseBuilder(
-//            appContext,
-//            JusicoolDataBase::class.java,
-//            "jusicool_database"
-//        ).build()
-//    }
-}
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object DatabaseModule {
+        @Provides
+        @Singleton
+        fun provideJusicoolDatabase(appContext: Context): JusicoolDataBase {
+            return Room.databaseBuilder(
+                appContext,
+                JusicoolDataBase::class.java,
+                "jusicool_database"
+            ).build()
+        }
+    }
