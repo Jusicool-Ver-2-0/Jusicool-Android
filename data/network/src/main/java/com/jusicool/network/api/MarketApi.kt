a915dcf30f496cb7587bf8cfb7c9fabc3c2bcf19
youloveli <mymhj122@gmail.com>
2025-07-02 19:46:02 +0900

    :recycle: :: MarketApi의 getMarketList의 엔드포인트를 holding -> market 으로 변경

--- data/network/src/main/java/com/jusicool/network/api/MarketApi.kt
+++ data/network/src/main/java/com/jusicool/network/api/MarketApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MarketApi {
-    @GET("/holding/list")
+    @GET("/market/list")
     suspend fun getMarketList(
         @Query("request_param") requestParam: String
     ): List<MarketListResponse>
