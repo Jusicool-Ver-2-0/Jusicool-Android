6a9967ed8229760573a3fa8ade010325d580933f
youloveli <mymhj122@gmail.com>
2025-07-03 09:25:25 +0900

    :sparkles: :: 주식의 현재가를 불러오는 GetStockCurrentPriceUseCase 구현

---
+++ domain/usecase/src/main/java/com/jusicool/usecase/koreaInvestment/GetStockCurrentPriceUseCase.kt
@@ -0,0 +1,29 @@
+package com.jusicool.usecase.koreaInvestment
+
+import com.jusicool.entity.price.AssetsCurrentPrice
+import com.jusicool.repository.KoreaInvestmentRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetStockCurrentPriceUseCase @Inject constructor(
+    private val koreaInvestmentRepository: KoreaInvestmentRepository,
+) {
+    operator fun invoke(
+        markets: List<String>,
+    ): Flow<List<AssetsCurrentPrice>> = flow {
+        val updatedMarkets = mutableListOf<AssetsCurrentPrice>()
+
+        markets.forEach { market ->
+            koreaInvestmentRepository.getStockCurrentPrice(
+                marketDivCode = "J",
+                stockCode = market
+            ).collect {
+                updatedMarkets.add(it)
+            }
+            emit(updatedMarkets)
+            delay(300)
+        }
+    }
+}
