8fd89ef236a79df4bfd6829baf0418a5b9fc1cec
youloveli <mymhj122@gmail.com>
2025-07-03 09:05:05 +0900

    :sparkles: :: StockPriceResponse -> AssetsCurrentPrice mapping 해주는 toEntity 함수 추가

---
+++ data/model/src/main/java/com/jusicool/model/mapper/koreaInvestment/StockPriceResponseMapper.kt
@@ -0,0 +1,11 @@
+package com.jusicool.model.mapper.koreaInvestment
+
+import StockPriceResponse
+import com.jusicool.entity.price.AssetsCurrentPrice
+
+fun StockPriceResponse.toEntity(): AssetsCurrentPrice =
+    AssetsCurrentPrice(
+        currentPrice = this.data.currentPrice.toIntOrNull() ?: 0,
+        priceDifference = this.data.priceDifference.toIntOrNull() ?: 0,
+        priceDifferenceRate = this.data.priceDifferenceRate.toDoubleOrNull() ?: 0.0
+    )
