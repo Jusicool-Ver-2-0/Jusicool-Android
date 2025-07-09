d14975d527a378cfcd6fc0fa1e12c6fbe2be3483
youloveli <mymhj122@gmail.com>
2025-07-09 16:39:41 +0900

    :lipstick: :: ChartItem에서 현재가와 상승률, 변동 가격을 띄우도록 수정

--- feature/investmentSearch/src/main/java/com/meister/investmentsearch/view/ChartListRoute.kt
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/view/ChartListRoute.kt
@@ -1,5 +1,6 @@
 package com.meister.investmentsearch.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -65,24 +67,28 @@ internal fun ChartListScreen(
     uiState: ChartListUiState,
     onSearchCLick: () -> Unit,
 ) {
-    Column(
-        modifier = modifier.fillMaxSize()
-    ) {
-        JusicoolTopBar(
-            modifier = Modifier.fillMaxWidth(),
-            startIcon = { UnionIcon() },
-            endIcon = {
-                SearchIcon(modifier = Modifier.clickable(onClick = onSearchCLick))
+    JusicoolTheme { colors, _ ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(colors.white),
+        ) {
+            JusicoolTopBar(
+                modifier = Modifier.fillMaxWidth(),
+                startIcon = { UnionIcon() },
+                endIcon = {
+                    SearchIcon(modifier = Modifier.clickable(onClick = onSearchCLick))
+                }
+            )
+
+            if (uiState.resentSearchTagData.isNotEmpty()) {
+                RecentSearchSection(data = uiState.resentSearchTagData)
+
+                Spacer(Modifier.height(16.dp))
             }
-        )
 
-        if (uiState.resentSearchTagData.isNotEmpty()) {
-            RecentSearchSection(data = uiState.resentSearchTagData)
-
-            Spacer(Modifier.height(16.dp))
+            ChartListSection(data = uiState.chartListData)
         }
-
-        ChartListSection(data = uiState.chartListData)
     }
 }
 
@@ -218,13 +224,14 @@ private fun ChartItem(data: ChartItemData) {
                 horizontalAlignment = Alignment.End,
             ) {
                 Text(
-                    text = data.priceChange.toSignedFormattedText(),
+                    text = "11,111,131 원",
                     style = typography.bodySmall,
-                    color = textColor
+                    color = colors.black,
+                    textAlign = TextAlign.End,
                 )
 
                 Text(
-                    text = "(${abs(data.percentageChange)}%)",
+                    text = "${data.priceChange.toSignedFormattedText()} (${abs(data.percentageChange)}%)",
                     style = typography.label,
                     color = textColor,
                 )
