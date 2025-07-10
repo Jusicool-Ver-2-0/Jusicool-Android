92033e4b32d9b43d2cd63ea9294cd98a268ed357
youloveli <mymhj122@gmail.com>
2025-07-10 14:44:50 +0900

    :rewind: :: resentSearchTagData의 값을 RecommendMarketWithPrice의 리스트에서 InvestmentSearchTagData의 리스트로 변경

--- feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/InvestmentSearchUiState.kt
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/InvestmentSearchUiState.kt
@@ -1,13 +1,13 @@
 package com.meister.investmentsearch.viewModel
 
-import com.jusicool.entity.market.RecommendMarketWithPrice
+import com.meister.investmentsearch.view.InvestmentSearchTagData
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 
 data class InvestmentSearchUiState(
     val isLoading: Boolean = true,
     val popularKeyword: String = "",
-    val resentSearchTagData: PersistentList<RecommendMarketWithPrice> = persistentListOf(),
+    val resentSearchTagData: PersistentList<InvestmentSearchTagData> = persistentListOf(),
     val popularKeywordData: PersistentList<Pair<String, Double>> = persistentListOf(),
     val errorMessage: String? = null,
 )
