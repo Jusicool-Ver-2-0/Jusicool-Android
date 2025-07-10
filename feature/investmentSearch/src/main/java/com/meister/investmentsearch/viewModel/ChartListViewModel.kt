dff2577f0445c09b87f2f64d62ddbf0d71f5d7af
youloveli <mymhj122@gmail.com>
2025-07-10 10:33:29 +0900

    :recycle: :: ChartListViewModel 의 uiState를 getTotalRecommendMarketListUseCase의 값을 사용하도록 변경

--- feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/ChartListViewModel.kt
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/ChartListViewModel.kt
@@ -1,14 +1,40 @@
 package com.meister.investmentsearch.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jusicool.usecase.market.GetTotalRecommendMarketListUseCase
+import com.jusicool.utils.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-internal class ChartListViewModel @Inject constructor() : ViewModel() {
-    private val _uiState = MutableStateFlow(ChartListUiState(isLoading = true))
-    val uiState: StateFlow<ChartListUiState> = _uiState.asStateFlow()
+internal class ChartListViewModel @Inject constructor(
+    getTotalRecommendMarketListUseCase: GetTotalRecommendMarketListUseCase
+) : ViewModel() {
+    val uiState: StateFlow<ChartListUiState> =
+        getTotalRecommendMarketListUseCase()
+            .map { recommendMarketList ->
+                ChartListUiState(
+                    isLoading = false,
+                    chartListData = recommendMarketList.toPersistentList(),
+                    errorMessage = null,
+                )
+            }
+            .onEach { Logger.d("ChartListViewModel", it.toString()) }
+            .catch { e ->
+                Logger.e("ChartListViewModel", "Error fetching holdings price at ChartListViewModel.kt:22", e)
+                emit(ChartListUiState(errorMessage = e.message))
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+                initialValue = ChartListUiState()
+            )
 }
