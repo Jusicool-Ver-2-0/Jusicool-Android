dffab223a9773627c8fcc149b62bae1bd4043c1a
youloveli <mymhj122@gmail.com>
2025-07-10 14:30:38 +0900

    :sparkles: :: InvestmentSearchUiState에서 searchTextState를 삭제하고 resentSearchTagData의 타입을 PersistentList<InvestmentSearchTagData>에서 PersistentList<RecommendMarketWithPrice>으로 변경

--- feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/InvestmentSearchUiState.kt
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/InvestmentSearchUiState.kt
@@ -1,13 +1,13 @@
 package com.meister.investmentsearch.viewModel
 
+import com.jusicool.entity.market.RecommendMarketWithPrice
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 
 data class InvestmentSearchUiState(
-    val isLoading: Boolean = false,
-    val searchTextState: String = "",
+    val isLoading: Boolean = true,
     val popularKeyword: String = "",
-    val resentSearchTagData: PersistentList<com.meister.investmentsearch.view.InvestmentSearchTagData> = persistentListOf(),
+    val resentSearchTagData: PersistentList<RecommendMarketWithPrice> = persistentListOf(),
     val popularKeywordData: PersistentList<Pair<String, Double>> = persistentListOf(),
     val errorMessage: String? = null,
 )
