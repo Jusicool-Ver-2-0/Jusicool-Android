9b17e5b73630a0f0d8cfeaace780a931ff56ca08
youloveli <mymhj122@gmail.com>
2025-07-10 10:08:06 +0900

    :recycle: :: GetCryptoRecommendMarketListUseCase 의 반환값을 Flow<List<RecommendMarketWithPrice>>으로 변경

--- domain/usecase/src/main/java/com/jusicool/usecase/market/GetCryptoRecommendMarketListUseCase.kt
+++ domain/usecase/src/main/java/com/jusicool/usecase/market/GetCryptoRecommendMarketListUseCase.kt
@@ -1,14 +1,18 @@
 package com.jusicool.usecase.market
 
-import com.jusicool.entity.market.Market
 import com.jusicool.entity.market.MarketType
+import com.jusicool.entity.market.RecommendMarketWithPrice
 import com.jusicool.repository.MarketRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetCryptoRecommendMarketListUseCase @Inject constructor(
     private val marketRepository: MarketRepository,
 ) {
-    operator fun invoke(): Flow<List<Market>> =
+    operator fun invoke(): Flow<List<RecommendMarketWithPrice>> = flow {
         marketRepository.getMarketList(MarketType.CRYPTO)
+        // TODO: 코인 현재가 불러오는 로직 추가 
+        emptyList<RecommendMarketWithPrice>()   
+    }
 }
