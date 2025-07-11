24a9ff563db3d51f37ae2a9df0946bb9a394c065
youloveli <mymhj122@gmail.com>
2025-07-11 15:57:57 +0900

    :sparkles: :: NavDestination에 investmentSearchRoute 등록

--- feature/investmentSearch/src/main/java/com/meister/investmentsearch/navigation/Navigation.kt
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/navigation/Navigation.kt
@@ -5,8 +5,10 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.meister.investmentsearch.view.ChartListRoute
+import com.meister.investmentsearch.view.InvestmentSearchRoute
 
 const val chartListRoute = "chartListRoute"
+const val investmentSearchRoute = "investmentSearchRoute"
 
 fun NavController.navigateToChartListRoute(
     navOptions: NavOptions? = null
@@ -14,10 +16,26 @@ fun NavController.navigateToChartListRoute(
     this.navigate(chartListRoute, navOptions)
 }
 
+fun NavController.navigateToInvestmentSearchRoute(
+    navOptions: NavOptions? = null
+) {
+    this.navigate(investmentSearchRoute, navOptions)
+}
+
 fun NavGraphBuilder.chartListRoute(
     navigateToSearchInvestmentRoute: () -> Unit,
 ) {
     composable(route = chartListRoute) {
         ChartListRoute(onSearchClick = navigateToSearchInvestmentRoute)
+    }
+}
+
+fun NavGraphBuilder.investmentSearchRoute(
+    popBackStack: () -> Unit,
+) {
+    composable(route = investmentSearchRoute) {
+        InvestmentSearchRoute(
+            popBackStack= popBackStack,
+        )
     }
 }
