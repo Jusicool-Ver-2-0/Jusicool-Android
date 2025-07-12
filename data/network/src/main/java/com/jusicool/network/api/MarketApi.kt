cb1c056b95917fe6206c0396e4cb32a56abd9fef
youloveli <mymhj122@gmail.com>
2025-07-13 02:22:24 +0900

    :recycle: :: getMarketList의 type 변경

--- data/network/src/main/java/com/jusicool/network/api/MarketApi.kt
+++ data/network/src/main/java/com/jusicool/network/api/MarketApi.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Query
 interface MarketApi {
     @GET("/market/list")
     suspend fun getMarketList(
-        @Query("request_param") requestParam: String
+        @Query("type") requestParam: String
     ): List<MarketListResponse>
 }
