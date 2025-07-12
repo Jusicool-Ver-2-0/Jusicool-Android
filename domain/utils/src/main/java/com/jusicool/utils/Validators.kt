99f53d8fdb4f71e3a3dc67381e25aad0d3576452
youloveli <mymhj122@gmail.com>
2025-07-13 02:30:55 +0900

    :recycle: :: 코드 판별식 변경

--- domain/utils/src/main/java/com/jusicool/utils/Validators.kt
+++ domain/utils/src/main/java/com/jusicool/utils/Validators.kt
@@ -1,9 +1,9 @@
 package com.jusicool.utils
 
 fun String.isValidCryptoMarketCode(): Boolean {
-    return this.matches(Regex("^[A-Z]{3,4}-[A-Z0-9]{2,10}$"))
+    return this.matches(Regex("^[A-Z]{2,5}-[A-Z0-9]{1,15}$"))
 }
 
 fun String.isValidStockMarketCode(): Boolean {
-    return this.matches(Regex("^\\d{6}$"))
+    return this.matches(Regex("^\\d{4,6}[A-Z0-9]{0,2}$"))
 }
