191f0555f274e9e3c51cb46a5b3ec9d7d234c69b
youloveli <mymhj122@gmail.com>
2025-07-16 20:05:33 +0900

    :sparkles: :: GetRecommendMarketPagingUseCase의 이름을 GetMarketListWithCurrentPriceUseCase으로 변경하고 FLow를 사용하고 Paging을 사용하지 않도록 수정

---
+++ domain/usecase/src/main/java/com/jusicool/usecase/market/GetMarketListWithCurrentPriceUseCase.kt
@@ -0,0 +1,16 @@
+package com.jusicool.usecase.market
+
+import com.jusicool.entity.market.MarketType
+import com.jusicool.entity.market.RecommendMarketWithPrice
+import com.jusicool.repository.MarketRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetMarketListWithCurrentPriceUseCase @Inject constructor(
+    private val marketRepository: MarketRepository,
+) {
+    operator fun invoke(type: MarketType): Flow<List<RecommendMarketWithPrice>> = flow {
+        marketRepository.getMarketList(type = type, page = 0, size = 40)
+    }
+}
