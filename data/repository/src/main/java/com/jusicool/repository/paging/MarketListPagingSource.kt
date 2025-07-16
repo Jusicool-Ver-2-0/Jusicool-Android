aa5af057694ff9984c95ab74d8c0fcb6995e59bb
youloveli <mymhj122@gmail.com>
2025-07-16 20:53:47 +0900

    :recycle: :: MarketListPagingSource에서 market의 현재가를 불러오도록 수정

--- data/repository/src/main/java/com/jusicool/repository/paging/MarketListPagingSource.kt
+++ data/repository/src/main/java/com/jusicool/repository/paging/MarketListPagingSource.kt
@@ -3,32 +3,61 @@ package com.jusicool.repository.paging
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.jusicool.entity.market.MarketType
-import com.jusicool.model.market.MarketResponse
+import com.jusicool.entity.market.RecommendMarketWithPrice
+import com.jusicool.model.mapper.market.toEntity
 import com.jusicool.network.datasource.market.MarketDataSource
+import com.jusicool.repository.CryptoRepository
+import com.jusicool.repository.KoreaInvestmentRepository
+import com.jusicool.usecase.market.util.mergeWithPrices
+import javax.inject.Inject
 
-class MarketListPagingSource(
-    private val dataSource: MarketDataSource,
-    private val type: MarketType
-) : PagingSource<Int, MarketResponse>() {
+class MarketListPagingSource @Inject constructor(
+    private val marketDataSource: MarketDataSource,
+    private val stockRepository: KoreaInvestmentRepository,
+    private val cryptoRepository: CryptoRepository,
+) : PagingSource<Int, RecommendMarketWithPrice>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MarketResponse> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, RecommendMarketWithPrice> {
+        val page = params.key ?: 1
+        val pageSize = params.loadSize
+
         return try {
-            val page = params.key ?: 1
-            val pageSize = params.loadSize
+            val response = marketDataSource.getMarketList(type = "", page = page, size = pageSize)
+            val items = response.items
+            val marketOrder = items.map { it.market }
 
-            val response = dataSource.getMarketList(type = type.name, page = page, size = pageSize)
+            val cryptoList = items.filter { it.marketType == MarketType.CRYPTO.name }
+            val stockList = items.filter { it.marketType == MarketType.STOCK.name }
+
+            val mergedRecommendList = mutableListOf<RecommendMarketWithPrice>()
+
+            stockRepository.getStockCurrentPrice(
+                marketDivCode = "J",
+                markets = stockList.map { it.market },
+            ).collect { stockPrices ->
+                mergedRecommendList += stockList.map { it.toEntity() }.mergeWithPrices(stockPrices)
+            }
+
+            cryptoRepository.getCurrentCryptoPrice(cryptoList.map { it.market })
+                .collect { cryptoPrices ->
+                    mergedRecommendList += cryptoList.map { it.toEntity() }
+                        .mergeWithPrices(cryptoPrices)
+                }
+
+            val sorted = mergedRecommendList.sortedBy { marketOrder.indexOf(it.market) }
 
             LoadResult.Page(
-                data = response.items,
+                data = sorted,
                 prevKey = if (page == 1) null else page - 1,
                 nextKey = if (response.hasNext) page + 1 else null
             )
+
         } catch (e: Exception) {
             LoadResult.Error(e)
         }
     }
 
-    override fun getRefreshKey(state: PagingState<Int, MarketResponse>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, RecommendMarketWithPrice>): Int? {
         return state.anchorPosition?.let { anchor ->
             val anchorPage = state.closestPageToPosition(anchor)
             anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
