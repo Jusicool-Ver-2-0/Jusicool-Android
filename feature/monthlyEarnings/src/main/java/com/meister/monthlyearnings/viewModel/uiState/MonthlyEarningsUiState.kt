6152acca3e6536aa288b048f71d3fb9658a35751
youloveli <mymhj122@gmail.com>
2025-07-17 13:38:20 +0900

    :sparkles: :: MonthlyEarningsUiState에 success 변환함수  추가

--- feature/monthlyEarnings/src/main/java/com/meister/monthlyearnings/viewModel/uiState/MonthlyEarningsUiState.kt
+++ feature/monthlyEarnings/src/main/java/com/meister/monthlyearnings/viewModel/uiState/MonthlyEarningsUiState.kt
@@ -1,8 +1,10 @@
 package com.meister.monthlyearnings.viewModel.uiState
 
 import com.jusicool.entity.order.DailyRate
+import com.jusicool.usecase.order.MonthlyRateGroup
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
 
 data class MonthlyEarningsUiState(
     val isLoading: Boolean = true,
@@ -12,4 +14,16 @@ data class MonthlyEarningsUiState(
     val stockHoldingsData: PersistentList<DailyRate> = persistentListOf(),
     val cryptoHoldingsData: PersistentList<DailyRate> = persistentListOf(),
     val errorMessage: String? = null,
-)
+) {
+    companion object {
+        fun success(data: MonthlyRateGroup): MonthlyEarningsUiState = MonthlyEarningsUiState(
+            isLoading = false,
+            monthlyEarnings = data.all.monthlyProfit(),
+            monthlyReturnRate = data.all.monthlyRate,
+            totalHoldingAssetsData = data.all.dailyRates.toPersistentList(),
+            stockHoldingsData = data.stock.dailyRates.toPersistentList(),
+            cryptoHoldingsData = data.crypto.dailyRates.toPersistentList(),
+            errorMessage = null
+        )
+    }
+}
