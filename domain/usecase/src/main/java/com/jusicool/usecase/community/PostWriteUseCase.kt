ab3dd849769f7ac1546ccf65bc0fbe62872b4efb
youloveli <mymhj122@gmail.com>
2025-08-18 20:02:50 +0900

    :recycle: :: PostWriteUseCase의 flow로 변경했습니다

--- domain/usecase/src/main/java/com/jusicool/usecase/community/PostWriteUseCase.kt
+++ domain/usecase/src/main/java/com/jusicool/usecase/community/PostWriteUseCase.kt
@@ -2,12 +2,12 @@ package com.jusicool.usecase.community
 
 import com.jusicool.model.community.WritePostRequest
 import com.jusicool.repository.CommunityRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class PostWriteUseCase @Inject constructor(
     private val communityRepository: CommunityRepository
 ) {
-    operator fun invoke(market: String, body: WritePostRequest) = runCatching {
+    operator fun invoke(market: String, body: WritePostRequest): Flow<Unit> =
         communityRepository.postWrite(market = market, body = body)
-    }
 }
