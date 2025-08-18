a54766d19ba0be7d4ae5bafa59d2944f5b3ca7e8
youloveli <mymhj122@gmail.com>
2025-08-18 20:04:16 +0900

    :recycle: :: SignInRequestUseCase의 반환값을 flow로 변경했습니다

--- domain/usecase/src/main/java/com/jusicool/usecase/auth/SignInRequestUseCase.kt
+++ domain/usecase/src/main/java/com/jusicool/usecase/auth/SignInRequestUseCase.kt
@@ -2,12 +2,12 @@ package com.jusicool.usecase.auth
 
 import com.jusicool.entity.auth.SignInModel
 import com.jusicool.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SignInRequestUseCase @Inject constructor(
     private val authRepository: AuthRepository
-){
-    operator fun invoke(body: SignInModel) = runCatching {
+) {
+    operator fun invoke(body: SignInModel): Flow<Unit> =
         authRepository.signIn(body = body)
-    }
 }
