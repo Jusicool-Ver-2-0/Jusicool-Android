8418ccc17efb062b260ceb63241dc113a90cd95f
youloveli <mymhj122@gmail.com>
2025-08-19 09:08:55 +0900

    :fire: :: CurrentMinuteCandleModel 삭제

--- domain/entity/src/main/java/com/jusicool/entity/crypto/CurrentMinuteCandleModel.kt
+++
@@ -1,9 +0,0 @@
-package com.jusicool.entity.crypto
-
-data class CurrentMinuteCandleModel (
-    val candleDateTimeKst: String,
-    val openingPrice: Double,
-    val highPrice: Double,
-    val lowPrice: Double,
-    val tradePrice: Double
-)
