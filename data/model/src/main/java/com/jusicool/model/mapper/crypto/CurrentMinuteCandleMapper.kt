ff4416a14d94d2744bcbb2f0f70a117f6a8f4ce2
youloveli <mymhj122@gmail.com>
2025-08-19 17:38:44 +0900

    :recycle: :: mapper에 parseDateTime 적용

--- data/model/src/main/java/com/jusicool/model/mapper/crypto/CurrentMinuteCandleMapper.kt
+++ data/model/src/main/java/com/jusicool/model/mapper/crypto/CurrentMinuteCandleMapper.kt
@@ -1,13 +1,15 @@
 package com.jusicool.model.mapper.crypto
 
-import com.jusicool.entity.price.MinuteCandleModel
+import com.jusicool.entity.price.MinuteCandleEntity
 import com.jusicool.model.crypto.CurrentMinuteCandleResponse
+import com.jusicool.utils.parseDateTime
 
-fun CurrentMinuteCandleResponse.toModel(): MinuteCandleModel =
-    MinuteCandleModel(
-        candleDateTimeKst = this.candleDateTimeKst,
-        openingPrice = this.openingPrice,
+fun CurrentMinuteCandleResponse.toModel(): MinuteCandleEntity =
+    MinuteCandleEntity(
+        dateTime = parseDateTime(candleDateTimeKst),
+        openPrice = this.openingPrice,
         highPrice = this.highPrice,
         lowPrice = this.lowPrice,
-        tradePrice = this.tradePrice
+        closePrice = this.tradePrice,
+        volume = 0.0 // TODO: 임시값 확인
     )
