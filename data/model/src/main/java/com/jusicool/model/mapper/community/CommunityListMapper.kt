9635705b466df25c8e9d6c61c4750e583b5a1da5
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-08-21 10:42:31 +0900

    ✨ :: CommunityListMapper 추가

---
+++ data/model/src/main/java/com/jusicool/model/mapper/community/CommunityListMapper.kt
@@ -0,0 +1,17 @@
+package com.jusicool.model.mapper.community
+
+import com.jusicool.entity.community.CommunityListModel
+import com.jusicool.model.community.CommunityListResponse
+
+fun CommunityListResponse.toModel(): CommunityListModel =
+    CommunityListModel(
+        id = this.id,
+        email = this.email,
+        market = this.market,
+        title = this.title,
+        content = this.content,
+        commentCount = this.commentCount,
+        likeCount = this.likeCount,
+        isLiked = this.isLiked,
+        isMine = this.isMine
+    )
