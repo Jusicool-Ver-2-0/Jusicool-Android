ff8afbc21caefdbd6c15b79dbcfb4f13658cc2a9
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-08-21 19:48:25 +0900

    ✨ :: SignUpDataSource 추가

--- data/network/src/main/java/com/jusicool/network/datasource/auth/AuthDataSource.kt
+++ data/network/src/main/java/com/jusicool/network/datasource/auth/AuthDataSource.kt
@@ -1,8 +1,11 @@
 package com.jusicool.network.datasource.auth
 
 import com.jusicool.model.auth.SignInRequest
+import com.jusicool.model.auth.SignUpRequest
 import kotlinx.coroutines.flow.Flow
 
 interface AuthDataSource {
     fun authSignIn(body: SignInRequest): Flow<Unit>
+
+    fun authSignUp(body: SignUpRequest): Flow<Unit>
 }
