e1f28caac94883676f89fd3aee77eae8604de3fd
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-08-21 19:49:21 +0900

    ✨ :: SignUpApi 추가

--- data/network/src/main/java/com/jusicool/network/api/AuthApi.kt
+++ data/network/src/main/java/com/jusicool/network/api/AuthApi.kt
@@ -3,10 +3,16 @@ package com.jusicool.network.api
 import retrofit2.http.Body
 import retrofit2.http.POST
 import com.jusicool.model.auth.SignInRequest
+import com.jusicool.model.auth.SignUpRequest
 
 interface AuthApi {
     @POST("/user/signin")
     suspend fun signIn(
         @Body body: SignInRequest
+    )
+
+    @POST("/user/signup")
+    suspend fun signUp(
+        @Body body: SignUpRequest
     )
 }
