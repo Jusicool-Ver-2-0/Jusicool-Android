2b0eab2d2d568fc0a16853f949644975b9e74533
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-08-25 15:36:53 +0900

    ✨ :: VerificationCodeRepository 추가

--- domain/repository/src/main/java/com/jusicool/repository/AuthRepository.kt
+++ domain/repository/src/main/java/com/jusicool/repository/AuthRepository.kt
@@ -2,6 +2,7 @@ package com.jusicool.repository
 
 import com.jusicool.entity.auth.SignInModel
 import com.jusicool.entity.auth.SignUpModel
+import com.jusicool.entity.auth.VerificationCodeModel
 import com.jusicool.entity.auth.VerificationEmailModel
 import com.jusicool.model.auth.VerificationEmailRequest
 import kotlinx.coroutines.flow.Flow
@@ -12,4 +13,6 @@ interface AuthRepository {
     fun signUp(body: SignUpModel): Flow<Unit>
 
     fun verificationEmail(body: VerificationEmailModel): Flow<Unit>
+
+    fun verificationCode(body: VerificationCodeModel): Flow<Unit>
 }
