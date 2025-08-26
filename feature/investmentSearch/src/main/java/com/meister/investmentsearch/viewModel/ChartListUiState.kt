4b75f16d3453c743b7690bf7f9e7eeda95b44809
youloveli <mymhj122@gmail.com>
2025-08-26 21:01:15 +0900

    :recycle: :: ChartListUiState에 페이징이 된 상태인지 확인하는 속성 추가

--- feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/ChartListUiState.kt
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/ChartListUiState.kt
@@ -7,6 +7,7 @@ import kotlinx.collections.immutable.persistentListOf
 
 data class ChartListUiState(
     val isLoading: Boolean = true,
+    val isPaging: Boolean = false,
     val resentSearchTagData: PersistentList<InvestmentSearchTagData> = persistentListOf(),
     val chartListData: PersistentList<RecommendMarketWithPrice> = persistentListOf(),
     val errorMessage: String? = null,
