bad59987fdd95e31b21e470bf20de3d42efaa022
youloveli <mymhj122@gmail.com>
2025-08-26 18:21:06 +0900

    :recycle: :: MarketDataSourceImpl, MarketDataSource에서의 getMarketList 함수가 Flow 값을 반환하도록 수정하고 suspend 함수에서 일반 함수로 수정

--- data/network/src/main/java/com/jusicool/network/datasource/market/MarketDataSourceImpl.kt
+++ data/network/src/main/java/com/jusicool/network/datasource/market/MarketDataSourceImpl.kt
@@ -3,7 +3,6 @@ package com.jusicool.network.datasource.market
 import com.jusicool.model.market.MarketListResponse
 import com.jusicool.model.market.MarketResponse
 import com.jusicool.network.api.MarketApi
-import com.jusicool.utils.JusicoolApiHandler
 import com.jusicool.utils.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -11,14 +10,12 @@ import javax.inject.Inject
 class MarketDataSourceImpl @Inject constructor(
     private val marketApi: MarketApi,
 ) : MarketDataSource {
-    override suspend fun getMarketList(
+    override fun getMarketList(
         type: String,
         page: Int,
         size: Int,
-    ): MarketListResponse =
-        JusicoolApiHandler<MarketListResponse>()
-            .httpRequest { marketApi.getMarketList(type = type, page = page, size = size) }
-            .sendRequest()
+    ): Flow<MarketListResponse> =
+        performApiRequest { marketApi.getMarketList(type = type, page = page, size = size) }
 
     override fun searchMarket(query: String): Flow<List<MarketResponse>> =
         performApiRequest { marketApi.searchMarket(query = query) }
