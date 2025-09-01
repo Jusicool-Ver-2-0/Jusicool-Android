9b1b0ae70ee2c8dd09dd750986e5bb1774490907
youloveli <mymhj122@gmail.com>
2025-09-01 20:22:46 +0900

    :recycle: :: ChartListUiState에서의 isLoading의 기본값을 false로 변경

--- feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/ChartListUiState.kt
+++ feature/investmentSearch/src/main/java/com/meister/investmentsearch/viewModel/ChartListUiState.kt
@@ -6,7 +6,7 @@ import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 
 data class ChartListUiState(
-    val isLoading: Boolean = true,
+    val isLoading: Boolean = false,
     val isInitialLoad: Boolean = true,
     val resentSearchTagData: PersistentList<InvestmentSearchTagData> = persistentListOf(),
     val chartListData: PersistentList<RecommendMarketWithPrice> = persistentListOf(),
