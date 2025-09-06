03262a853c7ee926359a2b4978851d0af8244f91
oheunchan07 <131967057+oheunchan07@users.noreply.github.com>
2025-09-07 00:58:37 +0900

    ✨ :: 의존성 추가

--- feature/signup/build.gradle.kts
+++ feature/signup/build.gradle.kts
@@ -13,4 +13,6 @@ dependencies {
     implementation(project(":domain:usecase"))
     implementation(project(":domain:entity"))
     implementation(project(":ui:utils"))
+    implementation(project(":domain:utils"))
+
 }
